7ebfad13e2af46c2ad94b26f1d5f873952cb2d7f
marsim86 <91328475+marsim86@users.noreply.github.com>
2021-10-15 15:22:18 +0200

    Add files via upload
    primera subida

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PRUEBA3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
