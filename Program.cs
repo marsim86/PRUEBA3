3c7e9bc6eeab9f6750bf5dbb880aa5f89c764aa6
marsim86 <marsim86@gmail.com>
2021-10-18 07:07:44 +0200

    fsadfsadf

--- Program.cs
+++ Program.cs
@@ -7,7 +7,16 @@ namespace PRUEBA3
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Servicio1 s1 = new Servicio1 ();
+            //Servicio1 s1 = new Servicio1 ();
+           // s1.call1 ();
+           RegValue rv = new RegValue ("BTC");
+           Random rnd = new Random();
+
+           for (int i=0;i<10;i++){
+               rv.setValue(rnd.NextDouble ());
+               Console.WriteLine ("Pinta valor " + i);
+               System.Threading.Thread.Sleep (1000);
+           }
             Console.WriteLine  ("hola");
         }
     }
--- Servicios/Servicio1.cs
+++ Servicios/Servicio1.cs
@@ -1,21 +1,68 @@
 using System;
+using System.IO;
 using System.Net;
 using System.Net.Http;
 
-class Servicio1{
-    public Servicio1(){
-        try{
-//HttpRequestMessage request = new HttpRequestMessage();
-        HttpRequestMessage request = new HttpRequestMessage();
-        request.Method = HttpMethod.Get;
-        request.RequestUri = new Uri("http:\\prescrivet.com");
-        request.Headers.Add("authorization", "Bearer 1234567890");
-        HttpResponseMessage response = new HttpClient().SendAsync(request).Result;
-        string responseString = response.Content.ReadAsStringAsync().Result;
-        Console.WriteLine(responseString);
-        }catch (Exception ex){
-Console.WriteLine("Error:" + ex.Message);
+class Servicio1
+{
+    public Servicio1()
+    {
+    }
+
+    public void call1()
+    {
+        try
+        {
+            //HttpRequestMessage request = new HttpRequestMessage();
+            HttpRequestMessage request = new HttpRequestMessage();
+            request.Method = HttpMethod.Post;
+            request.RequestUri = new Uri("https://reqbin.com/echo/post/json");
+            request.Headers.Add("authorization", "Bearer mt0dgHmLJMVQhvjpNXDyA83vA_PxH23Y");
+            HttpResponseMessage response = new HttpClient().SendAsync(request).Result;
+            string responseString = response.Content.ReadAsStringAsync().Result;
+            Console.WriteLine(responseString);
         }
-        
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error:" + ex.Message);
+        }
+    }
+
+    public void call2()
+    {
+        try
+        {
+            var url = "https://reqbin.com/echo/post/json";
+
+            var httpRequest = (HttpWebRequest)WebRequest.Create(url);
+            httpRequest.Method = "POST";
+
+            httpRequest.Headers["Authorization"] = "Bearer mt0dgHmLJMVQhvjpNXDyA83vA_PxH23Y";
+            httpRequest.ContentType = "application/json";
+
+            var data = @"{
+  ""Id"": 12345,
+  ""Customer"": ""John Smith"",
+  ""Quantity"": 1,
+  ""Price"": 10.00
+}";
+
+            using (var streamWriter = new StreamWriter(httpRequest.GetRequestStream()))
+            {
+                streamWriter.Write(data);
+            }
+
+            var httpResponse = (HttpWebResponse)httpRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                var result = streamReader.ReadToEnd();
+            }
+
+            Console.WriteLine(httpResponse.StatusCode);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Error:" + ex.Message);
+        }
     }
 }
